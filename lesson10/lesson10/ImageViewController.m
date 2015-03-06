c7317c92f739e6dadade3ebb9ce6b719849416e5
huliyou <liyoupie@gmail.com>
2015-03-06 09:15:34 +0800

    图片下载放入其他线程，不阻塞UI

--- lesson10/lesson10/ImageViewController.m
+++ lesson10/lesson10/ImageViewController.m
@@ -8,19 +8,59 @@
 
 #import "ImageViewController.h"
 
-@interface ImageViewController ()
+@interface ImageViewController () <UIScrollViewDelegate>
 @property (nonatomic,strong) UIImageView *imageView;
 @property (nonatomic,strong) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
 
 @implementation ImageViewController
+
+- (void)setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 1.2;
+    self.scrollView.delegate = self;
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
 - (void)setImageURL:(NSURL *)imageURL
 {
     _imageURL = imageURL;
-    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+//    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    [self startDownloadingImage];
 }
 
+- (void)startDownloadingImage
+{
+    self.image = nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request                    = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session                    = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+            if (!error) {
+                if ([request.URL isEqual:self.imageURL]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        self.image = image;
+                    });
+                }
+                
+            }
+        }];
+        [task resume];
+    }
+}
 
 - (UIImageView *)imageView
 {
@@ -38,12 +78,14 @@
 {
     self.imageView.image = image;
     [self.imageView sizeToFit];
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self.spinner stopAnimating]; 
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.view addSubview:self.imageView];
+    [self.scrollView addSubview:self.imageView];
 }
 
 
